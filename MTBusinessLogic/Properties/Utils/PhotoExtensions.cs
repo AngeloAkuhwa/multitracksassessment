5cdd8c41bceabfa219bf6920d718fcb90bc46424
AngeloAkuhwa <aakuhwa@appzonegroup.com>
2021-10-05 22:05:56 +0100

    implemented sign up and login functionalities

---
+++ MTBusinessLogic/Properties/Utils/PhotoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTBusinessLogic.Utils
+{
+    public class PhotoExtensions
+    {
+        public static string[] GetPhotoExtensions()
+        {
+         return ConfigurationManager.AppSettings["PhotoSettings:Extensions"].Split(',');
+        }
+
+        public static int GetPhotoLength()
+        {
+            int number;
+
+            int.TryParse(ConfigurationManager.AppSettings["PhotoSettings:photoLength"], out number);
+
+            return number;
+        }
+    }
+}
